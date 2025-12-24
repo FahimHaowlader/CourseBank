4000e1c1b800cc190bc3d51f6eed21a1bc9aab58
Fahim083 <fahimhaowlader07@gmail.com>
2025-12-25 02:00:18 +0600

    course page propto Type design done

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,23 +1,28 @@
 import './App.css'
-
 //import elements for npm  packages
 import { createBrowserRouter,RouterProvider,Navigate } from 'react-router'
 
 
+
 // import pages
-
 import CheckPage from './Pages/CheckPage.jsx'
-
-
-
+import CoursePage from './Pages/CoursePage.jsx'
 function App() {
+
+// const handleThemeChange = (event) => {
+//   const newTheme = event.target.checked ? "dark" : "light";
+//   setTheme(newTheme);
+//   localStorage.setItem("theme", newTheme);
+// };
+
   const router = createBrowserRouter([
-    { path: '/', element: <Navigate to="/home" replace /> },
+    { path: '/', element: <Navigate to="/courses" replace /> },
     {path:'/check-page', element: <CheckPage />},
+    {path:'/courses', element: <CoursePage />},
   ])
 
   return (
-    <div className="App">
+    <div className="App" >
       <RouterProvider router={router} />
     </div>
   )
--- FrontEnd/src/Pages/CheckPage.jsx
+++ FrontEnd/src/Pages/CheckPage.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+// import SelectBox from '../Components/SelectBox'
+// import InputBox from '../Components/InputBox'
 
 const CheckPage = () => {
   return (
     <div className='min-h-screen min-w-screen flex justify-center items-center' >
-      <h1>Hello world </h1>
+      <h1 className='text-primary border p-6 rounded-xl'>Hello world </h1>
+      {/* <SelectBox
+  label="Year"
+  options={["All Years", "2025", "2024", "2023"]}
+  value={['All Years']}
+  onChange={(v) => setFilters({ ...filters, year: v })}
+/> */}
+{/* <InputBox
+  label="Course Title"
+  icon="search"
+  placeholder="e.g. Intro to Computer Science"
+//   value={filters.title}
+  onChange={(e) =>
+    setFilters({ ...filters, title: e.target.value })
+  }
+/> */}
     </div>
   )
 }
