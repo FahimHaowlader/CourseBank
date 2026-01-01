a31e913491bc1d478e23b20b7117cffb3c181efd
Fahim083 <fahimhaowlader07@gmail.com>
2026-01-01 20:24:43 +0600

    fixing the the route

--- BackEnd/src/route.js
+++ BackEnd/src/route.js
@@ -1,89 +1,200 @@
 import { Router } from "express";
-import { userCourseSearch, fullCourseDetails,getCourseByCreatorId, createCourse, updateCourseInfo, uploadImage, uploadFile, deleteFile, updateCourseMaterials, updateCourseTasks, updateCourseAssessments, updateSuggestedBooks, updateCourseHandbook, deleteCourseHandbook, deleteCourse } from "../controllers/course.controller.js";
+import {
+  userCourseSearch,
+  fullCourseDetails,
+  getCourseByCreatorId,
+  createCourse,
+  updateCourseInfo,
+  uploadImage,
+  uploadFile,
+  deleteFile,
+  updateCourseMaterials,
+  updateCourseTasks,
+  updateCourseAssessments,
+  updateSuggestedBooks,
+  updateCourseHandbook,
+  deleteCourseHandbook,
+  deleteCourse
+} from "../controllers/course.controller.js";
 
+import {
+  createUser,
+  updateUserInfo,
+  userLogin,
+  deleteUser,
+  getAllUserSearch
+} from "../controllers/user.controller.js";
 
-import { createUser, updateUserInfo, userLogin, deleteUser, getAllUserSearch } from "../controllers/user.controller.js";
-
-import  verifyJwt from "../middlewares/auth.middleware.js";
-import {upload} from "../middlewares/upload.middleware.js";
-
-// Course Bank  version 1 apies
+import verifyJwt from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/upload.middleware.js";
 
 const router = Router();
 
+// -------------------- Public Routes --------------------
+router.get("/", (req, res) => res.send("API v1 is running"));
 
-// ----- define your routes here ----
+router.post("/login", async (req, res, next) => {
+  try {
+    await userLogin(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.route('/').get((req, res) => res.send('Api verson 1 is running'));   
+router.post("/upload/image", verifyJwt, upload.single("image"), async (req, res, next) => {
+  try {
+    await uploadImage(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
+router.post("/upload/file", verifyJwt, upload.single("file"), async (req, res, next) => {
+  try {
+    await uploadFile(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
-{/** helper route */}
-router.route('/login').post(userLogin);
+router.delete("/delete/file", verifyJwt, async (req, res, next) => {
+  try {
+    await deleteFile(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.route('/upload/image').post(verifyJwt, upload.single('image') , uploadImage);
+router.post("/users-all-course", async (req, res, next) => {
+  try {
+    await userCourseSearch(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.route('/upload/file').post(verifyJwt, upload.single('file') , uploadFile);
+router.get("/course-details/:courseId", async (req, res, next) => {
+  try {
+    await fullCourseDetails(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.route('/delete/file').delete(verifyJwt, deleteFile);
+// -------------------- Protected Routes (Moderators) --------------------
+router.use(verifyJwt);
 
-{/** users route */} 
+router.get("/courses-by-creator/:creatorId", async (req, res, next) => {
+  try {
+    await getCourseByCreatorId(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.route('/users-all-course').post(userCourseSearch);
-router.route('/course-details/:courseId').get(fullCourseDetails);
+router.post("/create-course", async (req, res, next) => {
+  try {
+    await createCourse(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
+router.patch("/update-course-info/:courseId", async (req, res, next) => {
+  try {
+    await updateCourseInfo(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
-{/** moderators route */} 
+router.patch("/update-course-materials/:courseId", async (req, res, next) => {
+  try {
+    await updateCourseMaterials(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.use(verifyJwt) ; // all routes below this line require authentication
+router.patch("/update-course-tasks/:courseId", async (req, res, next) => {
+  try {
+    await updateCourseTasks(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.route('/courses-by-creator/:creatorId').get(getCourseByCreatorId);
+router.patch("/update-course-assessments/:courseId", async (req, res, next) => {
+  try {
+    await updateCourseAssessments(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.route('/create-course').post(createCourse);
+router.patch("/update-suggested-books/:courseId", async (req, res, next) => {
+  try {
+    await updateSuggestedBooks(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.route('/update-course-info/:courseId').patch(updateCourseInfo);
+router.patch("/update-course-handbook/:courseId", async (req, res, next) => {
+  try {
+    await updateCourseHandbook(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.route('/update-course-materials/:courseId').patch(updateCourseMaterials);
+router.delete("/delete-course-handbook/:courseId", async (req, res, next) => {
+  try {
+    await deleteCourseHandbook(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.route('/update-course-tasks/:courseId').patch(updateCourseTasks);
+router.delete("/delete-course/:courseId", async (req, res, next) => {
+  try {
+    await deleteCourse(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.route('/update-course-assessments/:courseId').patch(updateCourseAssessments);
+// -------------------- Admin Routes --------------------
+router.post("/create-user", async (req, res, next) => {
+  try {
+    await createUser(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.route('/update-suggested-books/:courseId').patch(updateSuggestedBooks);
+router.patch("/update-user-info/:userId", async (req, res, next) => {
+  try {
+    await updateUserInfo(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.route('/update-course-handbook/:courseId').patch(updateCourseHandbook);
+router.delete("/delete-user/:userId", async (req, res, next) => {
+  try {
+    await deleteUser(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.route('/delete-course-handbook/:courseId').delete(deleteCourseHandbook);
-
-router.route('/delete-course/:courseId').delete(deleteCourse);
-
-
-
-{/** admin route */} 
-
-router.route('/create-user').post(createUser);
-
-router.route('/update-user-info/:userId').patch(updateUserInfo);
-
-router.route('/delete-user/:userId').delete(deleteUser);
-
-router.route('/search-users').get( getAllUserSearch);
-
-
-
-
+router.get("/search-users", async (req, res, next) => {
+  try {
+    await getAllUserSearch(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
 export default router;
-
-
-// when we will improve the Course Bank api then we can use this
-
-// Course Bank version 2 apies
-
-// const router = Router();
-// ----- define your routes here ----
-
-//  router.get('/', (req, res) => res.send('api verson 2 is running'));
-
-
-
-// export {router} ;
