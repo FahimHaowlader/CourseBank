9961d46ee926ac4df9dacf1282ffa63eda07ae67
Fahim083 <fahimhaowlader07@gmail.com>
2026-01-02 03:48:29 +0600

    changee tthe cors

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -24,21 +24,24 @@ const allowedOrigins = [
   process.env.CLIENT_URL_DEV,
 ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        return callback(new Error(`CORS blocked for origin: ${origin}`), false);
-      }
-      return callback(null, true);
-    },
-    credentials: true,
-    optionsSuccessStatus: 200,
-  })
-);
+// app.use(
+
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         return callback(new Error(`CORS blocked for origin: ${origin}`), false);
+//       }
+//       return callback(null, true);
+//     },
+//     credentials: true,
+//     optionsSuccessStatus: 200,
+//   })
+// );
+
 
 // -------------------- Body Parsing --------------------
+app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 
