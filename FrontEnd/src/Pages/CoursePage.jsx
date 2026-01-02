15cc1ce59bf9ec395b01f5ea53df472c1d1b9c36
Fahim083 <fahimhaowlader07@gmail.com>
2026-01-03 02:56:10 +0600

    course Page mvp Done

--- FrontEnd/src/Pages/CoursePage.jsx
+++ FrontEnd/src/Pages/CoursePage.jsx
@@ -507,7 +507,7 @@ const handleSortChange = (e) => {
                )
               }
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-           {loading && (
+          {loading && (
             <>
               <div>
               <SkeletonCard />
