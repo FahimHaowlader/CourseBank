5fa4eb9aa91308b66f313b0daed2ef89f088d927
Fahim083 <fahimhaowlader07@gmail.com>
2026-01-03 21:26:33 +0600

    update cors

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -15,22 +15,22 @@ const allowedOrigins = [
 ];
 
 
-app.use(cors()); // Enable CORS for all origins
+// app.use(cors()); // Enable CORS for all origins
 
 
-// app.use(cors({
-//     origin: function(origin, callback) {
-//         // allow requests with no origin (like mobile apps or curl)
-//         if (!origin) return callback(null, true);
-//         if (allowedOrigins.indexOf(origin) === -1) {
-//             const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
-//             return callback(new Error(msg), false);
-//         }
-//         return callback(null, true);
-//     },
-//     credentials: true,
-//     optionsSuccessStatus: 200 // for legacy browsers
-// }));
+app.use(cors({
+    origin: function(origin, callback) {
+        // allow requests with no origin (like mobile apps or curl)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true,
+    optionsSuccessStatus: 200 // for legacy browsers 
+}));
 
 
 app.use(express.json({ limit: '1mb' })); // Parse JSON request bodies
