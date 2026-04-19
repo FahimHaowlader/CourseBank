c242ea14e9890ef5fa3de9304a8a86405adf42dc
Fahim083 <fahimhaowlader07@gmail.com>
2026-04-20 00:58:58 +0600

    bug fixed in all course page

--- FrontEnd/src/Pages/AllCoursePage.jsx
+++ FrontEnd/src/Pages/AllCoursePage.jsx
@@ -31,15 +31,24 @@ const AllCoursePage = () => {
     status: '',
     courseCode: '',
     department: '',
-    degree: user?.degree || '',
+    degree: user.role === "moderator" ? user?.degree || '' : '',
     year: '',
-    semester: user?.semester || '',
+    semester: user.role === "moderator" ?  user?.semester || '' : '',
     type: '',
     credits: '',
     format: ''
   });
   // console.log("Initial Filters State:", filters); // Debugging line to check initial filters state
+      useEffect(() => {
+    // 1. Try scrolling the window
+    window.scrollTo({ top: 0, behavior: "smooth" });
 
+    // 2. Safety: Try scrolling the HTML element (for some mobile browsers)
+    document.documentElement.scrollTo({ top: 0, behavior: "smooth" });
+
+    // 3. Optional: If you have a specific container that scrolls, use:
+    // document.getElementById('main-container').scrollTo({ top: 0 });
+  }, [page]);
   // if (user) {
    const [sort, setSort] = useState({
     sortField: "staringDate",
